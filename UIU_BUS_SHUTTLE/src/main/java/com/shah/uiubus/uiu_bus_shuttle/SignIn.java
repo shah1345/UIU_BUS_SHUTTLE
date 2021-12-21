8d6cba70b452c677e93b8fc2e74d3799b4436f88
Shah Newaz <34304584+shah1345@users.noreply.github.com>
2021-12-21 22:55:55 +0600

    2.1

---
+++ UIU_BUS_SHUTTLE/src/main/java/com/shah/uiubus/uiu_bus_shuttle/SignIn.java
@@ -0,0 +1,22 @@
+package com.shah.uiubus.uiu_bus_shuttle;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class SignIn {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+
+
+    }
+
+}
